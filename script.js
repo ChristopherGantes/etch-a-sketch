4df84964b742f38207a27c7b37b11a1d10169bab
ChristopherGantes <78065304+ChristopherGantes@users.noreply.github.com>
2025-01-05 21:38:05 -0500

    added aesthetics such as red border and stretching to remove any whitespace

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 const container = document.querySelector("#container");
+const gridBtn = document.querySelector("#size");
 
-for (let i = 0; i < 16; i++) {
-  for (let j = 0; j < 16; j++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
-    square.setAttribute("id", `${i}x${j}`);
-    container.appendChild(square);
+function createGrid(rows, columns) {
+  container.textContent = "";
+  
+  for (let i = 0; i < rows; i++) {
+    const rowDiv = document.createElement("div");
+    rowDiv.setAttribute("id",`row`)
+
+    for (let j = 0; j < columns; j++) {
+      const square = document.createElement("div");
+      square.classList.add("square");
+      square.setAttribute("id", `${i}x${j}`);
+      rowDiv.appendChild(square);
+    }
+    
+    container.append(rowDiv);
   }
 }
+
+createGrid(16, 16);
 
 container.addEventListener("mouseover", (event) => {
   let target = event.target;
@@ -15,3 +27,10 @@ container.addEventListener("mouseover", (event) => {
 
   target.classList.add("hover");
 });
+
+
+gridBtn.onclick = () => {
+  const rows = prompt("Enter row size");
+  const columns = prompt("Enter column size");
+  createGrid(rows, columns);
+};
